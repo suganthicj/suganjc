547cd0d9526a852399dbe5e733f91fcf1d8eea6b
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-15 22:22:26 -0700

    Create suganjc.py

---
+++ suganjc.py
@@ -0,0 +1,11 @@
+tring = 'Geeksforgeeks'
+  
+print(string.isspace()) 
+   
+# checking if \n is a whitespace character 
+string = '\n \n \n'
+  
+print(string.isspace()) 
+  
+string = 'Geeks\nfor\ngeeks'
+print( string.isspace()) 
